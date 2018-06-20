27d0c611e09929e4e112087670b8da1d57b3a7a6
Edza <edexdev@gmail.com>
2018-06-20 23:24:04 +0300

    Updated

---
+++ precache-manifest.df8c90818a84dce1020f5c073834dc7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0022d7a9e612045adc8d",
+    "url": "/js/chunk-vendors.0022d7a9.js"
+  },
+  {
+    "revision": "5c5eeaa6d13a12ca9ee5",
+    "url": "/js/app.5c5eeaa6.js"
+  },
+  {
+    "revision": "4aad9d3b740dd7709f40aa8162f6825c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1e163e08f3a2cee9b941045cae900cd",
+    "url": "/img/logo.a1e163e0.jpg"
+  },
+  {
+    "revision": "5c5eeaa6d13a12ca9ee5",
+    "url": "/css/app.a03146d6.css"
+  }
+];
